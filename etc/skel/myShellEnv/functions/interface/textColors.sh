f2dec7d160ff08874f9eb160a3525feea5fb380a
AeonDigital <rianna.aeon@gmail.com>
2021-09-02 00:44:46 -0300

    Preparando alterações para uso de cores

--- etc/skel/myShellEnv/functions/interface/textColors.sh
+++ etc/skel/myShellEnv/functions/interface/textColors.sh
@@ -13,61 +13,32 @@ set +e
 #
 # 'D' indica 'Dark'
 # 'L' indica 'Light'
-# 'P' indica 'Prompt; para uso na construção de prompts'
 #
 
 NONE='\e[40;00m'
-#PNONE='\[\e[00m\]'
 
 BLACK='\e[40;00;30m'
-#PBLACK='\[\e[40;00;30m\]'
-
 DGREY='\e[40;01;30m'
-#PDGREY='\[\e[40;01;30m\]'
-
 WHITE='\e[40;00;37m'
-#PWHITE='\[\e[40;00;37m\]'
-
 SILVER='\e[40;01;37m'
-#PSILVER='\[\e[40;01;37m\]'
 
 RED='\e[40;00;31m'
-#PRED='\[\e[40;00;31m\]'
-
 LRED='\e[40;01;31m'
-#PLRED='\[\e[40;01;31m\]'
 
 GREEN='\e[40;00;32m'
-#PGREEN='\[\e[40;00;32m\]'
-
 LGREEN='\e[40;01;32m'
-#PLGREEN='\[\e[40;01;32m\]'
 
 BROWN='\e[40;00;33m'
-#PBROWN='\[\e[40;00;33m\]'
-
 YELLOW='\e[40;01;33m'
-#PYELLOW='\[\e[40;01;33m\]'
 
 BLUE='\e[40;00;34m'
-#PBLUE='\[\e[40;00;34m\]'
-
 LBLUE='\e[40;01;34m'
-#PLBLUE='\[\e[40;01;34m\]'
 
 PURPLE='\e[40;00;35m'
-#PPURPLE='\[\e[40;00;35m\]'
-
 LPURPLE='\e[40;01;35m'
-#PLPURPLE='\[\e[40;01;35m\]'
 
 CYAN='\e[40;00;36m'
-#PCYAN='\[\e[40;00;36m\]'
-
 LCYAN='\e[40;01;36m'
-#PLCYAN='\[\e[40;01;36m\]'
-
-
 
 # Atributo de fonte
 BOLD='\e[01m'
