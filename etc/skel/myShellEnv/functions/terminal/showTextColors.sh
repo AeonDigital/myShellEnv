2c7a120a6a5066f21bfc29507e689b59bc3e2a50
AeonDigital <rianna.aeon@gmail.com>
2021-09-02 02:26:43 -0300

    Preparando alterações para uso de cores

--- etc/skel/myShellEnv/functions/terminal/showTextColors.sh
+++ etc/skel/myShellEnv/functions/terminal/showTextColors.sh
@@ -30,6 +30,7 @@ showTextColors() {
   local mseColorName=""
   local mseColorRaw=""
   local mseColorCod=""
+  local mseLine=""
 
 
   for (( i=0; i<mseLength; i++)); do
@@ -37,33 +38,13 @@ showTextColors() {
     mseColorRaw=${MSE_GB_AVAILABLE_COLORS_RAW[$i]}
     mseColorCod="\\${!mseColorRaw}"
 
-    mseRawTable="${mseRawTable}${mseColorName}:${mseColorRaw}:${mseColorCod}:${!mseColorRaw}myShellEnv${NONE} \n"
+    mseLine="${mseColorName}:${mseColorRaw}:${mseColorCod}:${!mseColorRaw}myShellEnv${NONE} \n"
+    mseRawTable+="${mseLine}"
   done
 
-  ' :
-  local mseRawTable=$(printf "
-    Normal:         NONE:     \\\\e[40;00m:    ${NONE}myShellEnv \n
-    Preto:          BLACK:    \\\\e[40;00;30m: ${BLACK}myShellEnv${NONE} \n
-    Cinza escuro:   DGREY:    \\\\e[40;01;30m: ${DGREY}myShellEnv${NONE} \n
-    Branco:         WHITE:    \\\\e[40;00;37m: ${WHITE}myShellEnv${NONE} \n
-    Prata:          SILVER:   \\\\e[40;01;37m: ${SILVER}myShellEnv${NONE} \n
-    Vermelho:       RED:      \\\\e[40;00;31m: ${RED}myShellEnv${NONE} \n
-    Vermelho claro: LRED:     \\\\e[40;01;31m: ${LRED}myShellEnv${NONE} \n
-    Verde:          GREEN:    \\\\e[40;00;32m: ${GREEN}myShellEnv${NONE} \n
-    Verde claro:    LGREEN:   \\\\e[40;01;32m: ${LGREEN}myShellEnv${NONE} \n
-
-    Marrom:         BROWN:    \\\\e[40;00;33m: ${BROWN}myShellEnv${NONE} \n
-    Amarelo:        YELLOW:   \\\\e[40;01;33m: ${YELLOW}myShellEnv${NONE} \n
-    Azul:           BLUE:     \\\\e[40;00;34m: ${BLUE}myShellEnv${NONE} \n
-    Azul claro:     LBLUE:    \\\\e[40;01;34m: ${LBLUE}myShellEnv${NONE} \n
-    Purpura:        PURPLE:   \\\\e[40;00;35m: ${PURPLE}myShellEnv${NONE} \n
-    Purpura claro:  LPURPLE:  \\\\e[40;01;35m: ${LPURPLE}myShellEnv${NONE} \n
-    Ciano:          CYAN:     \\\\e[40;00;36m: ${CYAN}myShellEnv${NONE} \n
-    Ciano claro:    LCYAN:    \\\\e[40;01;36m: ${LCYAN}myShellEnv${NONE} \n
-  ")
-  '
-
   printf "\n\n${SILVER}As seguintes opções de cores estão disponíveis:${NONE} \n\n"
+
+  mseRawTable=$(printf "${mseRawTable}")
   mseRawTable=$(sed 's/^\s*//g' <<< "${mseRawTable}" | sed 's/\s*$//g' | sed 's/\s*:/:/g' | sed 's/:\s*/:/g')
 
   if [ $# == 0 ] || [ $1 == 0 ]; then
