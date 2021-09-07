a7a0d8a98bf5a496064da0506d9ce76904c51c5e
AeonDigital <rianna.aeon@gmail.com>
2021-09-06 22:00:28 -0300

    Incrementadas funções de conversão

--- etc/skel/myShellEnv/functions/tools/convertDecimalToChar.sh
+++ etc/skel/myShellEnv/functions/tools/convertDecimalToChar.sh
@@ -37,14 +37,15 @@ convertDecimalToChar() {
     local i
     local mseIsValid=1
     local mseRawDec=(${1// / })
-    local mseChar
+    local mseRawOctal
+    local mseTmp
 
 
     #
     # verifica cada um dos decimais apresentados identificando se
     # são válidos
     local mseREG='^[0-9]+$'
-    for (( i=0; i<${#mseRawDec}; i++ )); do
+    for (( i=0; i<${#mseRawDec[@]}; i++ )); do
       if [ $mseIsValid == 1 ]; then
         if ! [[ ${mseRawDec[$i]} =~ $mseREG ]]; then
           mseIsValid=0
@@ -55,48 +56,26 @@ convertDecimalToChar() {
 
 
     if [ $mseIsValid == 1 ]; then
-      local mseRawOctal=''
 
       #
       # Converte cada um dos decimais apresentados
       # em seu correspondente octal
-      for (( i=0; i<${#mseRawDec}; i++ )); do
-        if [ "$i" != "0" ]; then
-          mseRawOctal+=' '
-        fi
-
-        mseRawOctal+='\\'
-        mseRawOctal+=$(convertDecimalToOctal ${mseRawDec[$i]})
+      for (( i=0; i<${#mseRawDec[@]}; i++ )); do
+        mseTmp=$(convertDecimalToOctal ${mseRawDec[$i]})
+        mseRawOctal+="\\${mseTmp}"
       done
+
+      printf $mseRawOctal
+
+      #
+      # Adiciona o caracter de 'nova linha' caso necessário
+      if [ $# == 1 ]; then
+        printf "\n"
+      else
+        if [ $# == 2 ] && [ $2 == 0 ]; then
+          printf "\n"
+        fi
+      fi
     fi
-
-    printf $mseRawOctal
-
-
-
-    #local mseREG='^[0-9]+$'
-    #if ! [[ $1 =~ $mseREG ]]; then
-    #  errorAlert "${FUNCNAME[0]}" "argument 1 is not an valid decimal"
-    #else
-    #  if [ $1 -lt 33 ]; then
-    #    errorAlert "${FUNCNAME[0]}" "argument 1 must be greater than 32"
-    #  else
-
-        #
-        # Efetivamente converte o valor e imprime ele na tela.
-    #    printf '%02x' $1 | xxd -p -r | iconv -f 'CP437//' -t 'UTF-8'
-
-
-        #
-        # Adiciona o caracter de 'nova linha' caso necessário
-    #    if [ $# == 1 ]; then
-    #      printf "\n"
-    #    else
-    #      if [ $# == 2 ] && [ $2 == 0 ]; then
-    #        printf "\n"
-    #      fi
-    #    fi
-    #  fi
-    #fi
   fi
 }
