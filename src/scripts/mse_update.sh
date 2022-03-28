1c76a53582e5eec2495e630bfba49838315b654e
Rianna Cantarelli <rianna.aeon@gmail.com>
2022-03-28 19:21:33 -0300

    Segunda revisão

--- src/scripts/mse_update.sh
+++ src/scripts/mse_update.sh
@@ -12,7 +12,10 @@
 # Instala atualizações do 'myShellEnv', se houverem.
 mse_update() {
   if [ $EUID == 0 ]; then
+    printf "===== UPDATE 'myShellEnv' =====\n"
     git -C "${HOME}/.myShellEnv" pull
     git -C "${HOME}/.myShellEnv" submodule update --remote
+    printf "===== UPDATE 'myShellEnv' END =====\n"
+    . ~/.bashrc
   fi
 }
